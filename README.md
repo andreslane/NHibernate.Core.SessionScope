b1dc46c485d9f83e68c92b78785e3b72384a6388
Infrabel <dotnet@infrabel.be>
2014-12-29 16:29:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+GNaP.Data.Scope.NHibernate
+==========================
+
+## Copyright
+
+Copyright © 2014 Infrabel and contributors.
+
+## License
+
+GNaP.Data.Scope.NHibernate is licensed under [BSD (3-Clause)](http://choosealicense.com/licenses/bsd-3-clause/ "Read more about the BSD (3-Clause) License"). Refer to [LICENSE](https://github.com/infrabel/GNaP.Data.Scope.NHibernate/blob/master/LICENSE) for more information.
---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+ 
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("GNaP.Data.Scope.NHibernate")]
+[assembly: AssemblyDescription("A simple and flexible way to manage your NHibernate ISession instances.")]
+[assembly: AssemblyCopyright("Copyright (c) 2014 Infrabel")]
+[assembly: AssemblyCompany("Infrabel")]
+[assembly: AssemblyConfiguration("Release")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
